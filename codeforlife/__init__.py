57885901673da5ab53bda19866173277755a0492
KamilPawel <70412950+KamilPawel@users.noreply.github.com>
2023-04-04 11:39:40 +0100

    feat: user package (#8)
    * added user package with a simple test creating its instance
    
    * altered a comment
    
    * refactor quick save
    
    * rename cfl_user to user
    
    * remove unnecessary make_password
    
    * check migrations
    
    * adjust job triggers
    
    * fix code formatting
    
    * test everything
    
    * create docs folder
    
    * updated docs for the django model graphing
    
    * sudo apt-get install graphviz
    
    * remove unnecessary comments
    
    Co-Authored-By: SKairinos <stefan.kairinos@ocado.com>

--- codeforlife/__init__.py
+++ codeforlife/__init__.py
@@ -6,3 +6,5 @@ DATA_DIR = BASE_DIR.joinpath("data")
 
 
 from .version import __version__
+
+from . import user
---
+++ codeforlife/user/models/teacher.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from .user import User
+from .school import School
+
+
+class TeacherModelManager(models.Manager):
+    def factory(self, first_name, last_name, email, password):
+        user = User.objects.create_user(
+            username=email, email=email, password=password, first_name=first_name, last_name=last_name
+        )
+
+        return Teacher.objects.create(user=user)
+
+    # Filter out non active teachers by default
+    def get_queryset(self):
+        return super().get_queryset().filter(user__is_active=True)
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, related_name="teacher", null=True, blank=True, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, related_name="school_teacher", null=True, blank=True, on_delete=models.SET_NULL)
+    is_admin = models.BooleanField(default=False)
+    blocked_time = models.DateTimeField(null=True, blank=True)
+    invited_by = models.ForeignKey(
+        "self", related_name="invited_teachers", null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    objects = TeacherModelManager()
+
+    def teaches(self, userprofile):
+        if hasattr(userprofile, "student"):
+            student = userprofile.student
+            return not student.is_independent() and student.class_field.teacher == self
+
+    def has_school(self):
+        return self.school is not (None or "")
+
+    def has_class(self):
+        return self.class_teacher.exists()
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
