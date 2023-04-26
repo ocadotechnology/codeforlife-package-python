5af5316edf01cfe5d0083377f509838ac905c90a
SKairinos <stefan.kairinos@ocado.com>
2023-04-26 14:48:44 +0100

    fix: backpack check

--- codeforlife/kurono/avatar_state.py
+++ codeforlife/kurono/avatar_state.py
@@ -10,7 +10,7 @@ def create_avatar_state(avatar_state_json: Dict):
     avatar_state_dict["location"] = Location(**avatar_state_json["location"])
 
     # check backpack as it doesn't always exist (i.e. worksheet 1)
-    if "backpack" in avatar_state_json:
+    if avatar_state_json.get("backpack"):
         # use namedtuple for artefacts to allow accessing fields by name
         avatar_state_dict["backpack"] = Backpack(
             [namedtuple("Artefact", artefact.keys())(*artefact.values()) for artefact in avatar_state_json["backpack"]]
