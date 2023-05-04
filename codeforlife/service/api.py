98951e487110be96f26bf8993d3876be0063f498
SKairinos <stefan.kairinos@ocado.com>
2023-05-04 10:49:02 +0100

    fix: remove code exec from source

--- codeforlife/service/api.py
+++ codeforlife/service/api.py
@@ -4,21 +4,34 @@ from flask import request, Response
 from pydantic import BaseModel, ValidationError
 
 
+RequestJson = t.Dict[str, t.Any]
 RequestBody = t.TypeVar("RequestBody", bound=BaseModel)
 ResponseBody = t.TypeVar("ResponseBody", bound=BaseModel)
 
 
-def handle_flask_request(
+def validate_flask_request_json(
     request_body_type: t.Type[RequestBody],
-    run: t.Callable[[RequestBody], ResponseBody],
+    request_json: RequestJson,
 ):
     try:
-        request_body = request_body_type(**request.json)
+        return request_body_type(**request_json)
     except ValidationError as error:
         return Response(error.json(), status=400, content_type="application/json")
 
+
+def handle_flask_request(
+    run: t.Callable[[RequestBody], ResponseBody],
+    request_body_type: t.Type[RequestBody],
+    request_json: RequestJson = None,
+):
+    request_json = request_json or request.json
+
+    request_body_or_response = validate_flask_request_json(request_body_type, request_json)
+    if isinstance(request_body_or_response, Response):
+        return request_body_or_response
+
     try:
-        response_body = run(request_body)
+        response_body = run(request_body_or_response)
     except Exception as ex:
         return Response(str(ex), status=500, content_type="text/plain")
 
--- codeforlife/service/interfaces/kurono_badges.py
+++ codeforlife/service/interfaces/kurono_badges.py
@@ -4,62 +4,38 @@ from inspect import signature
 from pydantic import (
     BaseModel,
     Field,
-    PrivateAttr,
     validator,
 )
 
-from ...kurono import (
-    schema,
-    direction,
-    location,
-    MoveAction,
-    PickupAction,
-    WaitAction,
-    MoveTowardsAction,
-    DropAction,
-)
+from ...kurono import schema
 
 
 class RequestBody(BaseModel):
     class Source(BaseModel):
         code: str = Field()
-        _globals: t.Dict[str, t.Any] = PrivateAttr(
-            default_factory=lambda: {
-                "direction": direction,
-                "location": location,
-                "MoveAction": MoveAction,
-                "PickupAction": PickupAction,
-                "WaitAction": WaitAction,
-                "MoveTowardsAction": MoveTowardsAction,
-                "DropAction": DropAction,
-            }
-        )
-        _locals: t.Dict[str, t.Any] = PrivateAttr(default_factory=dict)
+        globals: t.Optional[t.Dict[str, t.Any]] = Field()
+        locals: t.Optional[t.Dict[str, t.Any]] = Field()
+
+        @validator("locals")
+        def defines_next_turn(cls, locals: t.Dict[str, t.Any]):
+            next_turn = locals.get("next_turn")
+            if not next_turn:
+                raise ValueError("next_turn is not defined")
+            if not callable(next_turn):
+                raise ValueError("next_turn is not a callable")
+
+            next_turn_parameters = signature(next_turn).parameters
+            if len(next_turn_parameters) != 2:
+                raise ValueError("next_turn expected 2 parameters")
+            if list(next_turn_parameters.keys()) != ["world_state", "avatar_state"]:
+                raise ValueError("next_turn has the wrong named parameters")
+
+            return locals
 
     source: Source = Field()
     current_avatar_id: int = Field()
     task_id: t.Optional[int] = Field(ge=1)
     game_state: schema.GameState = Field()
-
-    @validator("source")
-    def defines_next_turn(cls, source: Source):
-        exec(source.code, source._globals, source._locals)
-        for key in [key for key in source._locals.keys() if key != "next_turn"]:
-            source._globals[key] = source._locals.pop(key)
-
-        next_turn = source._locals.get("next_turn")
-        if not next_turn:
-            raise ValueError("next_turn is not defined")
-        if not callable(next_turn):
-            raise ValueError("next_turn is not a callable")
-
-        next_turn_parameters = signature(next_turn).parameters
-        if len(next_turn_parameters) != 2:
-            raise ValueError("next_turn expected 2 parameters")
-        if list(next_turn_parameters.keys()) != ["world_state", "avatar_state"]:
-            raise ValueError("next_turn has the wrong named parameters")
-
-        return source
 
 
 class ResponseBody(BaseModel):
