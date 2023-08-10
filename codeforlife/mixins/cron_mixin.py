a68bb02d70b949eecf9c83bdc797cefa6aab1975
SKairinos <skairinos@gmail.com>
2023-08-10 12:45:17 +0300

    add get method signature

--- codeforlife/mixins/cron_mixin.py
+++ codeforlife/mixins/cron_mixin.py
@@ -1,3 +1,6 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from ..permissions import IsCronRequestFromGoogle
 
 
@@ -9,3 +12,6 @@ class CronMixin:
 
     http_method_names = ["get"]
     permission_classes = [IsCronRequestFromGoogle]
+
+    def get(self, request: Request) -> Response:
+        raise NotImplementedError()
