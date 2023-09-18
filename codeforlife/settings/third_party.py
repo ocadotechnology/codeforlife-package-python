9853424d0f1470ab9cd0139e8ad5f9d1d5963c58
SKairinos <skairinos@gmail.com>
2023-09-18 13:08:34 +0100

    fix: cors settings

--- codeforlife/settings/third_party.py
+++ codeforlife/settings/third_party.py
@@ -1,3 +1,12 @@
 """
 This file contains custom settings defined by third party extensions.
 """
+
+from .django import DEBUG
+
+# CORS
+# https://pypi.org/project/django-cors-headers/
+
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = ["https://codeforlife.education"]
