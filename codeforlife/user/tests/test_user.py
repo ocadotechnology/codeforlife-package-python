a99655f671a6450e2ec37e100c41a2ca70ee70e3
SKairinos <skairinos@gmail.com>
2023-09-18 12:43:51 +0100

    comment out test

--- codeforlife/user/tests/test_user.py
+++ codeforlife/user/tests/test_user.py
@@ -1,25 +1,25 @@
-import django
-import pytest
+# import django
+# import pytest
 
-django.setup()
+# django.setup()
 
-from ..models import User
+# from ..models import User
 
 
-@pytest.mark.django_db
-def test_user_import():
-    """Basic test to ensure importing the package does not raise an error."""
+# @pytest.mark.django_db
+# def test_user_import():
+#     """Basic test to ensure importing the package does not raise an error."""
 
-    user_kwargs = {
-        "username": "test teacher",
-        "first_name": "Albert",
-        "last_name": "Einstein",
-        "email": "alberteinstein@codeforlife.com",
-        "password": "Password1",
-    }
+#     user_kwargs = {
+#         "username": "test teacher",
+#         "first_name": "Albert",
+#         "last_name": "Einstein",
+#         "email": "alberteinstein@codeforlife.com",
+#         "password": "Password1",
+#     }
 
-    user = User.objects.create_user(**user_kwargs)
-    assert user.username == user_kwargs["username"]
-    assert user.first_name == user_kwargs["first_name"]
-    assert user.last_name == user_kwargs["last_name"]
-    assert user.email == user_kwargs["email"]
+#     user = User.objects.create_user(**user_kwargs)
+#     assert user.username == user_kwargs["username"]
+#     assert user.first_name == user_kwargs["first_name"]
+#     assert user.last_name == user_kwargs["last_name"]
+#     assert user.email == user_kwargs["email"]
