1c8ebcad218e02b2cea797af15ddc9791d22ab01
SKairinos <skairinos@gmail.com>
2023-09-18 15:08:39 +0100

    fix: add urls helper

---
+++ codeforlife/urls.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import include, path, re_path
+from rest_framework import status
+
+from .settings import SERVICE_IS_ROOT, SERVICE_NAME
+
+
+def service_urlpatterns(
+    api_urls_path: str = "api.urls",
+    frontend_template_name: str = "frontend.html",
+):
+    urlpatterns = [
+        path(
+            "admin/",
+            admin.site.urls,
+            name="admin",
+        ),
+        path(
+            "api/",
+            include(api_urls_path),
+            name="api",
+        ),
+        re_path(
+            r"^api/.*",
+            lambda request: HttpResponse(
+                "API endpoint not found",
+                status=status.HTTP_404_NOT_FOUND,
+            ),
+            name="api-endpoint-not-found",
+        ),
+        re_path(
+            r".*",
+            lambda request: render(request, frontend_template_name),
+            name="frontend",
+        ),
+    ]
+
+    if SERVICE_IS_ROOT:
+        return urlpatterns
+
+    return [
+        path(
+            f"{SERVICE_NAME}/",
+            include(urlpatterns),
+            name="service",
+        ),
+        re_path(
+            r".*",
+            lambda request: HttpResponse(
+                f'The base route is "{SERVICE_NAME}/".',
+                status=status.HTTP_404_NOT_FOUND,
+            ),
+            name="service-not-found",
+        ),
+    ]
