88b3ecfe9da94170650423ccb66a042b97e155bd
Stefan Kairinos <118008817+SKairinos@users.noreply.github.com>
2023-10-12 09:56:47 +0100

    fix: session cookie and drf request (#16)

--- codeforlife/request.py
+++ codeforlife/request.py
@@ -3,6 +3,7 @@ import typing as t
 from django.contrib.auth.models import AnonymousUser
 from django.core.handlers.wsgi import WSGIRequest as _WSGIRequest
 from django.http import HttpRequest as _HttpRequest
+from rest_framework.request import Request as _Request
 
 from .user.models import User
 from .user.models.session import SessionStore
@@ -14,5 +15,10 @@ class WSGIRequest(_WSGIRequest):
 
 
 class HttpRequest(_HttpRequest):
+    session: SessionStore
+    user: t.Union[User, AnonymousUser]
+
+
+class Request(_Request):
     session: SessionStore
     user: t.Union[User, AnonymousUser]
