d1aaf51ba5496755cdc59cce2daf0b8f76bcf104
SKairinos <stefan.kairinos@ocado.com>
2024-02-08 11:37:29 +0000

    feat: is_self permission

--- codeforlife/permissions/__init__.py
+++ codeforlife/permissions/__init__.py
@@ -7,4 +7,5 @@ Reusable DRF permissions.
 
 from .allow_none import AllowNone
 from .is_cron_request_from_google import IsCronRequestFromGoogle
+from .is_self import IsSelf
 from .operators import AND, NOT, OR, Permission
