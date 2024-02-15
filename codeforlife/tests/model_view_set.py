d16c2301d6e7bc7d9b1ed48bf887a23cc5bbe82f
SKairinos <stefan.kairinos@ocado.com>
2024-02-15 17:03:30 +0000

    fix: use legacy assertQuerysetEqual

--- codeforlife/tests/model_view_set.py
+++ codeforlife/tests/model_view_set.py
@@ -776,7 +776,7 @@ class ModelViewSetTestCase(APITestCase, t.Generic[AnyModel]):
         model_view_set = self.model_view_set_class(*args, **kwargs)
         queryset = model_view_set.get_queryset()
         # pylint: disable-next=no-member
-        self.assertQuerySetEqual(queryset, values, ordered=ordered)
+        self.assertQuerysetEqual(queryset, values, ordered=ordered)
 
     def get_other_user(
         self,
