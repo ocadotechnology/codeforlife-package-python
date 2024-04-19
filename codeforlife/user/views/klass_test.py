3ffbf1361f389f5c1a83e93835c1b4da79e69bcd
SKairinos <stefan.kairinos@ocado.com>
2024-04-19 18:12:03 +0000

    fix remaining view tests

--- codeforlife/user/views/klass_test.py
+++ codeforlife/user/views/klass_test.py
@@ -5,72 +5,28 @@ Created on 20/01/2024 at 09:48:30(+00:00).
 
 from ...permissions import OR
 from ...tests import ModelViewSetTestCase
-from ..models import Class, User
+from ..models import (
+    AdminSchoolTeacherUser,
+    Class,
+    NonAdminSchoolTeacherUser,
+    StudentUser,
+    User,
+)
 from ..permissions import IsStudent, IsTeacher
 from ..views import ClassViewSet
 
 RequestUser = User
 
 
-# pylint: disable-next=too-many-ancestors
+# pylint: disable-next=too-many-ancestors,missing-class-docstring
 class TestClassViewSet(ModelViewSetTestCase[RequestUser, Class]):
-    """
-    Base naming convention:
-        test_{action}
-
-    action: The view set action.
-        https://www.django-rest-framework.org/api-guide/viewsets/#viewset-actions
-    """
-
     basename = "class"
     model_view_set_class = ClassViewSet
 
-    def _login_student(self):
-        return self.client.login_student(
-            first_name="Leonardo",
-            password="Password1",
-            class_id="AB123",
-        )
-
-    # pylint: disable-next=pointless-string-statement
-    """
-    Retrieve naming convention:
-        test_retrieve__{user_type}__{same_school}__{in_class}
-
-    user_type: The type of user that is making the request. Options:
-        - teacher: A non-admin teacher.
-        - admin_teacher: An admin teacher.
-        - student: A school student.
-        - indy_student: A non-school student.
-
-    same_school: A flag for if the class is in the same school that the user is
-        in. Options:
-        - same_school: The class is in the same school as the user.
-        - not_same_school: The class is not in the same school as the user.
-
-    in_class: A flag for if the user is in the class. Options:
-        - in_class: The user is in the class.
-        - not_in_class: The user is not in the class.
-    """
-
-    def test_retrieve__student__same_school__in_class(self):
-        """
-        Student can retrieve a class from the same school and a class they are
-        in.
-        """
-
-        user = self._login_student()
-
-        self.client.retrieve(user.student.class_field)
-
-    # TODO: other retrieve and list tests
-    # TODO: replace above tests with get_queryset() tests
+    # test: get permissions
 
     def test_get_permissions__list(self):
-        """
-        Only school-teachers can list classes.
-        """
-
+        """Only school-teachers can list classes."""
         self.assert_get_permissions(
             permissions=[
                 OR(IsTeacher(is_admin=True), IsTeacher(in_class=True))
@@ -79,10 +35,7 @@ class TestClassViewSet(ModelViewSetTestCase[RequestUser, Class]):
         )
 
     def test_get_permissions__retrieve(self):
-        """
-        Anyone in a school can retrieve a class.
-        """
-
+        """Anyone in a school can retrieve a class."""
         self.assert_get_permissions(
             permissions=[
                 OR(
@@ -91,4 +44,40 @@ class TestClassViewSet(ModelViewSetTestCase[RequestUser, Class]):
                 )
             ],
             action="retrieve",
+        )
+
+    # test: get queryset
+
+    def test_get_queryset__student(self):
+        """Student-users can only target classes they are in."""
+        user = StudentUser.objects.first()
+        assert user
+
+        self.assert_get_queryset(
+            values=[user.student.class_field],
+            request=self.client.request_factory.get(user=user),
+        )
+
+    def test_get_queryset__teacher__admin(self):
+        """
+        Admin-teacher-users can only target all the classes in their school.
+        """
+        user = AdminSchoolTeacherUser.objects.first()
+        assert user
+
+        self.assert_get_queryset(
+            values=user.teacher.classes,
+            request=self.client.request_factory.get(user=user),
+        )
+
+    def test_get_queryset__teacher__non_admin(self):
+        """
+        Non-admin-teacher-users can only target all the classes they teach.
+        """
+        user = NonAdminSchoolTeacherUser.objects.first()
+        assert user
+
+        self.assert_get_queryset(
+            values=user.teacher.classes,
+            request=self.client.request_factory.get(user=user),
         )
