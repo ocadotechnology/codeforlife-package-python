be81148996a51269ff25c245f4ba945e279afa8d
Stefan Kairinos <stefan.kairinos@ocado.com>
2024-04-24 14:01:04 +0100

    fix: polish tests (#114)
    * fix: add test coverage
    
    * use xml
    
    * rename codecov yaml
    
    * add yml path
    
    * ignore codeforlife test helpers
    
    * reset pipeline
    
    * reset pipeline
    
    * use updated workflow
    
    * polish_test
    
    * add todo
    
    * Merge branch 'main' into polish_tests
    
    * fix: test job
    
    * use new test workflow
    
    * add some small tests and don't cover some lines
    
    * no cov when debugging tests
    
    * test get queryset
    
    * update config
    
    * merge from main
    
    * use main
    
    * add test file
    
    * Merge branch 'main' into polish_tests
    
    * change
    
    * echo repo owner id
    
    * test if
    
    * only assert project
    
    * test user serializer and view
    
    * fix remaining view tests
    
    * test actions
    
    * use correct test job
    
    * remove unused imports
    
    * Merge branch 'main' into polish_tests
    
    * don't allow code coverage to drop
    
    * fix: coverage settings

---
+++ codeforlife/user/serializers/user_test.py
@@ -0,0 +1,59 @@
+"""
+© Ocado Group
+Created on 18/04/2024 at 17:26:59(+01:00).
+"""
+
+from ...tests import ModelSerializerTestCase
+from ..models import IndependentUser, StudentUser, TeacherUser, User
+from .user import UserSerializer
+
+
+# pylint: disable-next=missing-class-docstring
+class TestUserSerializer(ModelSerializerTestCase[User, User]):
+    model_serializer_class = UserSerializer
+
+    # test: to representation
+
+    def test_to_representation__teacher(self):
+        """Serialize teacher user to representation."""
+        user = TeacherUser.objects.first()
+        assert user
+
+        self.assert_to_representation(
+            user,
+            new_data={
+                "teacher": {
+                    "id": user.teacher.id,
+                    "school": user.teacher.school.id,
+                    "is_admin": user.teacher.is_admin,
+                },
+                "student": None,
+            },
+        )
+
+    def test_to_representation__student(self):
+        """Serialize student user to representation."""
+        user = StudentUser.objects.first()
+        assert user
+
+        self.assert_to_representation(
+            user,
+            new_data={
+                "teacher": None,
+                "student": {
+                    "id": user.student.id,
+                    "klass": user.student.class_field.access_code,
+                    "school": user.student.class_field.teacher.school.id,
+                },
+            },
+        )
+
+    def test_to_representation__indy(self):
+        """Serialize independent user to representation."""
+        user = IndependentUser.objects.first()
+        assert user
+
+        self.assert_to_representation(
+            user,
+            new_data={"teacher": None, "student": None},
+        )
--- codeforlife/user/views/school_test.py
+++ codeforlife/user/views/school_test.py
@@ -3,202 +3,65 @@
 Created on 20/01/2024 at 09:47:30(+00:00).
 """
 
-from rest_framework import status
 
 from ...permissions import OR, AllowNone
 from ...tests import ModelViewSetTestCase
-from ..models import Class, School, Student, Teacher, User, UserProfile
+from ..models import School, SchoolTeacherUser, StudentUser, User
 from ..permissions import IsStudent, IsTeacher
 from ..views import SchoolViewSet
 
 RequestUser = User
 
 
-# pylint: disable-next=too-many-ancestors
+# pylint: disable-next=too-many-ancestors,missing-class-docstring
 class TestSchoolViewSet(ModelViewSetTestCase[RequestUser, School]):
-    """
-    Base naming convention:
-        test_{action}
-
-    action: The view set action.
-        https://www.django-rest-framework.org/api-guide/viewsets/#viewset-actions
-    """
-
     basename = "school"
     model_view_set_class = SchoolViewSet
 
-    # TODO: replace this setup with data fixtures.
-    def setUp(self):
-        school = School.objects.create(
-            name="ExampleSchool",
-            country="UK",
-        )
-
-        user = User.objects.create(
-            first_name="Example",
-            last_name="Teacher",
-            email="example.teacher@codeforlife.com",
-            username="example.teacher@codeforlife.com",
-        )
-
-        user_profile = UserProfile.objects.create(user=user)
-
-        teacher = Teacher.objects.create(
-            user=user_profile,
-            new_user=user,
-            school=school,
-        )
-
-        klass = Class.objects.create(
-            name="ExampleClass",
-            teacher=teacher,
-            created_by=teacher,
-        )
-
-        user = User.objects.create(
-            first_name="Example",
-            last_name="Student",
-            email="example.student@codeforlife.com",
-            username="example.student@codeforlife.com",
-        )
-
-        user_profile = UserProfile.objects.create(user=user)
-
-        Student.objects.create(
-            class_field=klass,
-            user=user_profile,
-            new_user=user,
-        )
-
-    def _login_admin_school_teacher(self):
-        return self.client.login_admin_school_teacher(
-            email="alberteinstein@codeforlife.com",
-            password="Password1",
-        )
-
-    def _login_student(self):
-        return self.client.login_student(
-            first_name="Leonardo",
-            password="Password1",
-            class_id="AB123",
-        )
-
-    def _login_indy(self):
-        return self.client.login_indy(
-            email="indianajones@codeforlife.com",
-            password="Password1",
-        )
-
-    # pylint: disable-next=pointless-string-statement
-    """
-    Retrieve naming convention:
-        test_retrieve__{user_type}__{same_school}
-
-    user_type: The type of user that is making the request. Options:
-        - teacher: A teacher.
-        - student: A school student.
-        - indy_student: A non-school student.
-
-    same_school: A flag for if the school is the same school that the user
-        is in. Options:
-        - same_school: The other user is from the same school.
-        - not_same_school: The other user is not from the same school.
-    """
-
-    def test_retrieve__indy_student(self):
-        """
-        Independent student cannot retrieve any school.
-        """
-
-        self._login_indy()
-
-        school = School.objects.first()
-        assert school
-
-        self.client.retrieve(school, status.HTTP_403_FORBIDDEN)
-
-    def test_retrieve__teacher__same_school(self):
-        """
-        Teacher can retrieve the same school they are in.
-        """
-
-        user = self._login_admin_school_teacher()
-
-        self.client.retrieve(user.teacher.school)
-
-    def test_retrieve__student__same_school(self):
-        """
-        Student can retrieve the same school they are in.
-        """
-
-        user = self._login_student()
-
-        self.client.retrieve(user.student.class_field.teacher.school)
-
-    def test_retrieve__teacher__not_same_school(self):
-        """
-        Teacher cannot retrieve a school they are not in.
-        """
-
-        user = self._login_admin_school_teacher()
-
-        school = School.objects.exclude(id=user.teacher.school.id).first()
-        assert school
-
-        self.client.retrieve(school, status.HTTP_404_NOT_FOUND)
-
-    def test_retrieve__student__not_same_school(self):
-        """
-        Student cannot retrieve a school they are not in.
-        """
-
-        user = self._login_student()
-
-        school = School.objects.exclude(
-            id=user.student.class_field.teacher.school.id
-        ).first()
-        assert school
-
-        self.client.retrieve(school, status.HTTP_404_NOT_FOUND)
-
-    # pylint: disable-next=pointless-string-statement
-    """
-    List naming convention:
-        test_list__{user_type}
-
-    user_type: The type of user that is making the request. Options:
-        - teacher: A teacher.
-        - student: A school student.
-        - indy_student: A non-school student.
-    """
-
-    def test_list__indy_student(self):
-        """
-        Independent student cannot list any schools.
-        """
-
-        self._login_indy()
-
-        self.client.list([], status.HTTP_403_FORBIDDEN)
-
-    # TODO: replace above tests with get_queryset() tests
+    # test: get permissions
 
     def test_get_permissions__list(self):
-        """
-        No one is allowed to list schools.
-        """
-
+        """No one can list schools."""
         self.assert_get_permissions(
             permissions=[AllowNone()],
             action="list",
         )
 
     def test_get_permissions__retrieve(self):
-        """
-        Only a user in a school can retrieve a school.
-        """
-
+        """Only student and school-teachers can retrieve a school."""
         self.assert_get_permissions(
             permissions=[OR(IsStudent(), IsTeacher(in_school=True))],
             action="retrieve",
         )
+
+    # test: get queryset
+
+    def test_get_queryset__teacher(self):
+        """A school-teacher-user can only target the school they are in."""
+        user = SchoolTeacherUser.objects.first()
+        assert user
+
+        self.assert_get_queryset(
+            values=[user.teacher.school],
+            request=self.client.request_factory.get(user=user),
+        )
+
+    def test_get_queryset__student(self):
+        """A student-user can only target the school they are in."""
+        user = StudentUser.objects.first()
+        assert user
+
+        self.assert_get_queryset(
+            values=[user.student.class_field.teacher.school],
+            request=self.client.request_factory.get(user=user),
+        )
+
+    # test: actions
+
+    def test_retrieve(self):
+        """Can successfully retrieve a school."""
+        user = SchoolTeacherUser.objects.first()
+        assert user
+
+        self.client.login_as(user, password="abc123")
+        self.client.retrieve(model=user.teacher.school)
