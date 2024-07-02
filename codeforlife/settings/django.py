5adf77a6874c36bf489f0e84d37cd1a69652a9a8
SKairinos <skairinos@gmail.com>
2024-07-02 15:04:16 +0000

    added common installed apps

--- codeforlife/settings/django.py
+++ codeforlife/settings/django.py
@@ -183,6 +183,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#installed-apps
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sites",
+    "django.contrib.staticfiles",
+    "aimmo",  # TODO: remove
+    "game",  # TODO: remove
+    "portal",  # TODO: remove
+    "common",  # TODO: remove
     "api",
     "codeforlife.user",
     "corsheaders",
