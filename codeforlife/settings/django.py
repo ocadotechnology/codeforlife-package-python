c07fee43885f95d4b8efb86d40ce492422f33fcf
SKairinos <skairinos@gmail.com>
2024-07-02 14:38:46 +0000

    simplify project setup

--- codeforlife/settings/django.py
+++ codeforlife/settings/django.py
@@ -144,7 +144,7 @@ LOGGING = {
 # URLs
 # https://docs.djangoproject.com/en/3.2/ref/settings/#root-urlconf
 
-ROOT_URLCONF = "src.urls"
+ROOT_URLCONF = "api.urls"
 
 # App
 # https://docs.djangoproject.com/en/3.2/ref/settings/#wsgi-application
@@ -183,9 +183,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#installed-apps
 
 INSTALLED_APPS = [
-    "src.api",
+    "api",
     "codeforlife.user",
     "corsheaders",
     "rest_framework",
     "django_filters",
 ]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
+def get_static_root(base_dir: Path):
+    """Get the static root for the Django project.
+
+    Args:
+        base_dir: The base directory of the Django project.
+
+    Returns:
+        The static root for the django project.
+    """
+    return base_dir / "static"
+
+
+STATIC_URL = "/static/"
