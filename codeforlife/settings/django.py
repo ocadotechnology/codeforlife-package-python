aa188efa4a35f84b90c7f748ab0844dee51aaea2
SKairinos <skairinos@gmail.com>
2024-07-02 15:12:52 +0000

    templates

--- codeforlife/settings/django.py
+++ codeforlife/settings/django.py
@@ -219,3 +219,22 @@ def get_static_root(base_dir: Path):
 
 
 STATIC_URL = "/static/"
+
+# Templates
+# https://docs.djangoproject.com/en/3.2/ref/templates/
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
