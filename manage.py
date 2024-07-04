5f1f589ac67558728b67f2bfeb01954011ef4958
SKairinos <skairinos@gmail.com>
2024-07-04 11:51:58 +0000

    remove duplicate settings

--- manage.py
+++ manage.py
@@ -35,28 +35,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "codeforlife.user.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = get_databases(BASE_DIR)
 
 if __name__ == "__main__":
     import os
