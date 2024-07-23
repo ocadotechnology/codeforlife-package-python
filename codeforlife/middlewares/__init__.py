b88adaa263cc07c92ba144fdcf0e8dc32afc0392
SKairinos <stefan.kairinos@ocado.com>
2024-07-23 14:05:28 +0000

    fix: delete session metadata cookie

---
+++ codeforlife/middlewares/__init__.py
@@ -0,0 +1,6 @@
+"""
+© Ocado Group
+Created on 23/07/2024 at 14:28:21(+01:00).
+"""
+
+from .session import SessionMiddleware
---
+++ codeforlife/middlewares/session.py
@@ -0,0 +1,34 @@
+"""
+© Ocado Group
+Created on 23/07/2024 at 14:18:28(+01:00).
+"""
+
+from django.conf import settings
+from django.contrib.sessions.middleware import (
+    SessionMiddleware as _SessionMiddleware,
+)
+
+
+class SessionMiddleware(_SessionMiddleware):
+    """
+    Override the session middleware to delete the session metadata cookie when
+    the session key cookie is deleted.
+    """
+
+    def process_response(self, request, response):
+        response = super().process_response(request, response)
+
+        session = response.cookies.get(settings.SESSION_COOKIE_NAME)
+        if (
+            session
+            and session.get("max-age") == 0
+            and session.get("expires") == "Thu, 01 Jan 1970 00:00:00 GMT"
+        ):
+            response.delete_cookie(
+                key=settings.SESSION_METADATA_COOKIE_NAME,
+                path=settings.SESSION_COOKIE_PATH,
+                domain=settings.SESSION_COOKIE_DOMAIN,
+                samesite=settings.SESSION_COOKIE_SAMESITE,
+            )
+
+        return response
