7202539343e50f1012442ba8da88c93f112f9cc8
SKairinos <stefan.kairinos@ocado.com>
2024-09-16 15:14:24 +0000

    fix sort

--- codeforlife/urls/__init__.py
+++ codeforlife/urls/__init__.py
@@ -4,4 +4,4 @@ Created on 16/09/2024 at 15:23:05(+01:00).
 """
 
 from .handlers import handler400, handler403, handler404, handler500
-from .patterns import get_urlpatterns, UrlPatterns
+from .patterns import UrlPatterns, get_urlpatterns
