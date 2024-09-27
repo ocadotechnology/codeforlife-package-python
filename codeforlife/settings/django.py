967d217099394488b6bc6d6c2f73037a971c82b1
SKairinos <skairinos@gmail.com>
2024-09-27 08:37:17 +0000

    default back to service name

--- codeforlife/settings/django.py
+++ codeforlife/settings/django.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 def get_databases(  # pragma: no cover
-    name: str = "db",
+    name: str = SERVICE_NAME,
     host: str = "localhost",
     port: int = 5432,
     user: str = "root",
