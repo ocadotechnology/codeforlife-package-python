f9294cd5f6a8b76874949a0e9bf2c521cd4733bb
SKairinos <stefan.kairinos@ocado.com>
2024-11-05 18:46:46 +0000

    import BaseModelListSerializer

--- codeforlife/serializers/__init__.py
+++ codeforlife/serializers/__init__.py
@@ -5,4 +5,4 @@ Created on 20/01/2024 at 11:19:12(+00:00).
 
 from .base import BaseSerializer
 from .model import BaseModelSerializer, ModelSerializer
-from .model_list import ModelListSerializer
+from .model_list import BaseModelListSerializer, ModelListSerializer
