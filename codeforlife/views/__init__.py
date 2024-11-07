9fc68d3c8a699f7edae8ae3e88b480e54ac5787f
SKairinos <stefan.kairinos@ocado.com>
2024-11-07 15:21:59 +0000

    base login view and form

--- codeforlife/views/__init__.py
+++ codeforlife/views/__init__.py
@@ -4,6 +4,7 @@ Created on 24/01/2024 at 13:07:38(+00:00).
 """
 
 from .api import APIView, BaseAPIView
+from .base_login import BaseLoginView
 from .common import CsrfCookieView, LogoutView
 from .decorators import action, cron_job
 from .model import BaseModelViewSet, ModelViewSet
