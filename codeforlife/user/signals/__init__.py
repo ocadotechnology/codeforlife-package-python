13cae058073b64cd6917b328ea3e9269eb47d4d9
SKairinos <skairinos@gmail.com>
2025-01-17 16:18:37 +0000

    fix: new otp secret

--- codeforlife/user/signals/__init__.py
+++ codeforlife/user/signals/__init__.py
@@ -4,5 +4,6 @@ Created on 14/03/2024 at 12:14:41(+00:00).
 """
 
 # NOTE: Need to import signals so they are discoverable by Django.
+from .auth_factor import auth_factor__post_delete
 from .teacher import teacher_receiver
 from .user import user_receiver
