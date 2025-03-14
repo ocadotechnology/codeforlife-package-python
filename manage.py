8ad0b061eea67e318c9214911f59936e03541511
SKairinos <skairinos@gmail.com>
2025-03-14 17:24:29 +0000

    clean up

--- manage.py
+++ manage.py
@@ -2,44 +2,27 @@
 © Ocado Group
 Created on 12/04/2024 at 16:53:51(+01:00).
 
-This file manages Django but also acts a settings file.
+Django's command-line utility for administrative tasks.
 """
 
-# pylint: disable-next=wildcard-import,unused-wildcard-import
-from codeforlife.settings import *
+import os
+import sys
 
-# NOTE: This is only used locally for testing purposes.
-SECRET_KEY = "XTgWqMlZCMI_E5BvCArkif9nrJIIhe_6Ic6Q_UcWJDk="
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.sites",
-    "codeforlife.user",
-    "game",  # TODO: remove this
-    "common",  # TODO: remove this
-    "portal",  # TODO: remove this
-]
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    try:
+        # pylint: disable-next=import-outside-toplevel
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-MIDDLEWARE = [
-    "codeforlife.middlewares.SessionMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-]
-
-ROOT_URLCONF = "codeforlife.user.urls"
 
 if __name__ == "__main__":
-    import os
-    import sys
-
-    from django.core.management import execute_from_command_line
-
-    # Set the path of the settings file to the path of this file.
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", Path(__file__).stem)
-
-    execute_from_command_line(sys.argv)
+    main()
---
+++ settings.py
@@ -0,0 +1,34 @@
+"""
+© Ocado Group
+Created on 14/03/2025 at 15:34:28(+00:00).
+
+The min settings required to manage the `codeforlife.user` Django app-module.
+"""
+
+# pylint: disable-next=wildcard-import,unused-wildcard-import
+from codeforlife.settings import *
+
+# NOTE: This is only used locally for testing purposes.
+SECRET_KEY = "XTgWqMlZCMI_E5BvCArkif9nrJIIhe_6Ic6Q_UcWJDk="
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "codeforlife.user",
+    "game",  # TODO: remove this
+    "common",  # TODO: remove this
+    "portal",  # TODO: remove this
+]
+
+MIDDLEWARE = [
+    "codeforlife.middlewares.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+ROOT_URLCONF = "codeforlife.user.urls"
