e38e2abb1e5ef6faaeb5bfebc1978efb088dfa81
SKairinos <skairinos@gmail.com>
2025-03-24 16:57:19 +0000

    class_access_code_validators

--- codeforlife/user/models/klass.py
+++ codeforlife/user/models/klass.py
@@ -5,8 +5,18 @@ Created on 19/02/2024 at 21:54:04(+00:00).
 
 # pylint: disable-next=unused-import
 from common.models import Class  # type: ignore[import-untyped]
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
-from ...validators import UnicodeAlphanumericCharSetValidator
+from ...validators import (
+    UnicodeAlphanumericCharSetValidator,
+    UppercaseAsciiAlphanumericCharSetValidator,
+)
+
+class_access_code_validators = [
+    MinLengthValidator(5),
+    MaxLengthValidator(5),
+    UppercaseAsciiAlphanumericCharSetValidator(),
+]
 
 class_name_validators = [
     UnicodeAlphanumericCharSetValidator(
