c78c58f6ace208b9309053faec07d48d9d9a11a8
SKairinos <skairinos@gmail.com>
2025-03-25 16:50:51 +0000

    fix: todo

--- codeforlife/user/models/school.py
+++ codeforlife/user/models/school.py
@@ -9,6 +9,7 @@ from common.models import School  # type: ignore[import-untyped]
 from ...types import Validators
 from ...validators import UnicodeAlphanumericCharSetValidator
 
+# TODO: add to School.name field-validators in new schema.
 school_name_validators: Validators = [
     UnicodeAlphanumericCharSetValidator(
         spaces=True,
