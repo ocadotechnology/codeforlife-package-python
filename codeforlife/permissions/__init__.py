1cb057ee10e34dc6f9a9807d8de9f2606051629c
SKairinos <skairinos@gmail.com>
2025-06-20 12:55:46 +0000

    include import

--- codeforlife/permissions/__init__.py
+++ codeforlife/permissions/__init__.py
@@ -7,6 +7,7 @@ Reusable DRF permissions.
 
 from .allow_any import AllowAny
 from .allow_none import AllowNone
+from .auth_header_is_github_oidc_token import AuthHeaderIsGitHubOidcToken
 from .base import BasePermission
 from .is_authenticated import IsAuthenticated
 from .operators import AND, NOT, OR, Permission
